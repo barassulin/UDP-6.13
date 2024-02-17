6a00a0315cefd3102d88536631ce02db7366fbd9
barassulin <148526209+barassulin@users.noreply.github.com>
2024-02-17 23:47:35 +0200

    Create secret_messages_server.py

---
+++ secret_messages_server.py
@@ -0,0 +1,30 @@
+"""
+Author: Bar Assulin
+Date: 25/1/24
+"""
+
+import sys
+from scapy.all import *
+import logging
+
+
+def empty_udp_filter(packet):
+    if packet.haslayer(UDP) and not packet[UDP].payload:
+        return True
+    else:
+        return False
+
+
+def main():
+    string = ""
+    while True:
+        capture = sniff(filter="udp", count=1, lfilter=empty_udp_filter)
+        char = chr(capture[0][UDP].dport)
+        string = string + char
+        print(string)
+
+
+if __name__ == "__main__":
+    #logging.basicConfig(format=LOG_FORMAT, filename=LOG_FILE, level=LOG_LEVEL)
+
+    main()
