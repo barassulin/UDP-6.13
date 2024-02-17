3833f535f783156d18352004a7be9c2e714415dd
barassulin <148526209+barassulin@users.noreply.github.com>
2024-02-17 23:46:51 +0200

    Create secret_messages.py

---
+++ secret_messages.py
@@ -0,0 +1,25 @@
+"""
+Author: Bar Assulin
+Date: 25/1/24
+"""
+
+import scapy
+import logging
+
+
+#contant
+MY_IP = "10.0.0.23"
+
+
+def main():
+    message = str(input("Enter your message: "))
+    for mes in message:
+        port = ord(mes)
+        send((IP(dst=MY_IP) / UDP(dport=port)))
+
+
+if __name__ == "__main__":
+    #logging.basicConfig(format=LOG_FORMAT, filename=LOG_FILE, level=LOG_LEVEL)
+
+
+    main()
